3c4568d419dbcd71e7c7bc47903dc2bfca023e5c
George Mathew <georgemathew@Georges-MacBook-Air.local>
2025-03-01 16:46:00 +0400

    Add Netlify and Vite config files

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
+}) 
